6901dde6976d1cb024145707da5c442d4195dd03
JsebasC <jhoansebastian231@hotmail.com>
2023-01-25 22:52:23 -0500

    Se habilitan solicitudes cross-origin

--- University.API/University.API/App_Start/WebApiConfig.cs
+++ University.API/University.API/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using University.API.Controllers;
 
 namespace University.API
@@ -10,7 +11,11 @@ namespace University.API
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
-        {
+        {            
+            var enableCorsAttribute = new EnableCorsAttribute("*","Origin, Content-Type,Accept","GET,PUT,POST,DELETE,OPTIONS");
+            config.EnableCors(enableCorsAttribute);
+
+
             // Web API configuration and services
 
             // Web API routes
